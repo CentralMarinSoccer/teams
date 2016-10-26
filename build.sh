b87c6060fd74bea35eb3cf6e1797ed4c36dbc9fc
Central Marin Soccer <rrobinett@centralmarinsoccer.com>
2016-10-26 08:24:50 -0700

    builds app, docker container and pushes to hub

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Cross compile app
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o teams . || { echo "Building Go App failed" ; exit 1; }
+
+# Build the docker container
+docker build -t centralmarinsoccer/teams . || { echo "Building Docker container failed" ; exit 1; }
+
+# Push to docker hub
+docker push centralmarinsoccer/teams || { echo "Pusing container to Docker hub failed" ; exit 1; }
