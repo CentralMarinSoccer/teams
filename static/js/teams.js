7171021be1cc888ed54edfb627394d934bc8efb0
Central Marin Soccer <rrobinett@centralmarinsoccer.com>
2016-11-03 06:40:18 -0700

    General cleanup

--- static/js/teams.js
+++ static/js/teams.js
@@ -1,4 +1,3 @@
-
 // TODO: Handle service down
 // TODO: Need to handle no dates (no scheduled events)
 
@@ -6,9 +5,9 @@
 //
 // Extend the Date object with some nice helpers
 //
-Date.prototype.displayTime = function() {
+Date.prototype.displayTime = function () {
     var me = this;
-    return function() {
+    return function () {
         var hour = me.getHours();
         var dd = " AM";
         if (hour > 12) {
@@ -20,50 +19,50 @@ Date.prototype.displayTime = function() {
     }();
 };
 
-Date.prototype.displayDate = function() {
+Date.prototype.displayDate = function () {
     var me = this;
-    return function() {
+    return function () {
         return (me.getMonth() + 1) + "/" + ("0" + me.getDate()).slice(-2) + "/" + me.getFullYear();
     }();
 };
 
-Date.prototype.justDate = function() {
+Date.prototype.justDate = function () {
     var me = this;
-    return function() {
+    return function () {
         return new Date(me.getFullYear(), me.getMonth(), me.getDate());
     }();
 };
 
 
-var Teams = (function() {
+var Teams = (function () {
 
     ////////////////////////////////////////////
     //
     // Locations is a collection of Location objects
     //
-    var Locations = (function() {
+    var Locations = (function () {
         var locations = {};
 
-        var _key = function(lat, lng) {
+        var _key = function (lat, lng) {
             return lat + "|" + lng;
         };
 
         return {
             // Adds a location if it doesn't already exist
-            addLocation: function(lat, lng, address, name) {
-                var l = locations[_key(lat,lng)];
+            addLocation: function (lat, lng, address, name) {
+                var l = locations[_key(lat, lng)];
                 if (l == undefined) {
                     // create the location
                     l = new Location(lat, lng, address, name);
-                    locations[_key(lat,lng)] = l;
+                    locations[_key(lat, lng)] = l;
                 }
 
                 return l;
             },
             // Get all markers for the location for the specified date
-            markers: function(dateObj) {
+            markers: function (dateObj) {
                 var markers = [];
-                for(var key in locations) {
+                for (var key in locations) {
                     var location = locations[key];
                     var marker = location.marker(dateObj);
                     if (marker != undefined) {
@@ -85,18 +84,18 @@ var Teams = (function() {
     //
     // Location encapsulates attributes, games, and a map marker
     //
-    var Location = (function(lat, lng, address, name) {
+    var Location = (function (lat, lng, address, name) {
         var _lat = lat;
         var _lng = lng;
         var _address = address;
         var _name = name;
         var _markers = {};
 
-        var _getInfo = function(marker) {
+        var _getInfo = function (marker) {
 
             var info = marker.info + "<ul>";
 
-            marker.games.sort(function(game1, game2) {
+            marker.games.sort(function (game1, game2) {
                 return game1.compare(game2);
             });
 
@@ -108,13 +107,13 @@ var Teams = (function() {
             return info;
         };
 
-        var _key = function(dateObj) {
+        var _key = function (dateObj) {
             return dateObj.displayDate();
         };
 
         return {
             // Adds a game to the current location
-            addGame: function(dateObj, teamName) {
+            addGame: function (dateObj, teamName) {
                 // check if the marker exists
                 var key = _key(dateObj);
                 var marker = _markers[key];
@@ -140,7 +139,7 @@ var Teams = (function() {
                 marker.games.push(new Game(dateObj, teamName));
             },
             // Retrieves a marker for a specified date
-            marker: function(dateObj) {
+            marker: function (dateObj) {
                 return _markers[_key(dateObj)];
             }
         };
@@ -150,14 +149,18 @@ var Teams = (function() {
     //
     // Game encapsulates game attributes and provides helpers to sort and convert to a display string
     //
-    var Game = (function(dateObj, teamName) {
+    var Game = (function (dateObj, teamName) {
         var _dateObj = dateObj;
         var _teamName = teamName;
 
         return {
-            compareString: function() { return _dateObj + _teamName; },
-            display: function() { return _dateObj.displayTime() + ": " + _teamName; },
-            compare: function(game) {
+            compareString: function () {
+                return _dateObj + _teamName;
+            },
+            display: function () {
+                return _dateObj.displayTime() + ": " + _teamName;
+            },
+            compare: function (game) {
                 var c1 = this.compareString();
                 var c2 = game.compareString();
                 if (c1 > c2) return 1;
@@ -171,37 +174,37 @@ var Teams = (function() {
     var _datePicker; // DOM object
     var _map;  // Google Map object
     var _locations = Locations; // All locations
-    var _teams  = []
+    var _teams = []
 
-    var teamsFn = doT.template(document.getElementById('teamstmpl').text)
+    var teamsFn = doT.template(document.getElementById('teamstmpl').text);
     var teamFn = doT.template(document.getElementById('teamtmpl').text);
 
-    var teamName = function(team) {
+    var teamName = function (team) {
         return team.year + " " + team.gender + " " + team.level;
     };
 
     // Create the XHR object.
     function createCORSRequest(method, url) {
-      var xhr = new XMLHttpRequest();
-      if ("withCredentials" in xhr) {
-        // XHR for Chrome/Firefox/Opera/Safari.
-        xhr.open(method, url, true);
-      } else if (typeof XDomainRequest != "undefined") {
-        // XDomainRequest for IE.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-      } else {
-        // CORS not supported.
-        xhr = null;
-      }
-      return xhr;
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            // XHR for Chrome/Firefox/Opera/Safari.
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest != "undefined") {
+            // XDomainRequest for IE.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            // CORS not supported.
+            xhr = null;
+        }
+        return xhr;
     }
 
-    var initMap = function() {
+    var initMap = function () {
 
         _infoWindow = new google.maps.InfoWindow();
-        _map = new google.maps.Map(document.getElementById('map'));
-        _datePicker = document.getElementById('map-date-picker');
+        _map = new google.maps.Map(document.getElementById('teams-map'));
+        _datePicker = document.getElementById('teams-map-date-picker');
 
         // load team data
         getTeamsJSON(function (data) {
@@ -222,31 +225,35 @@ var Teams = (function() {
             return;
         }
 
-        xhr.onload = function() {
+        xhr.onload = function () {
             data = JSON.parse(xhr.responseText);
-            successHandler && successHandler(data); 
+            successHandler && successHandler(data);
         };
 
-        xhr.onerror = function() {
-          errorHandler && errorHandler(xhr.status);
+        xhr.onerror = function () {
+            errorHandler && errorHandler(xhr.status);
         };
 
         xhr.send();
     };
 
-    var processTeams = function(data) {
+    var processTeams = function (data) {
 
         data.teams.forEach(function (team) {
             _teams.push(team);
         });
 
         // Sort teams to facilitate generating data structure
-        _teams.sort(function(team1, team2) {
+        _teams.sort(function (team1, team2) {
             if (team1.year > team2.year) return -1;
             if (team1.year < team2.year) return 1;
 
             if (team1.gender > team2.gender) return 1;
             if (team1.gender < team2.gender) return -1;
+
+            // Custom Sorting for CM
+            if (team1.level == 'Black') return 1;
+            if (team2.level == 'Black') return -1;
 
             if (team1.level > team2.level) return 1;
             if (team1.level < team2.level) return -1;
@@ -257,24 +264,45 @@ var Teams = (function() {
         // generate data structure
         var years = [];
         var year = {};
-        _teams.forEach(function(team, i) {
+        _teams.forEach(function (team, i) {
             if (team.year != year.name) {
-                year = {}
+                year = {};
                 year.name = team.year;
-                year.boys = []
-                year.girls = []
+                year.boys = [];
+                year.girls = [];
                 years.push(year)
             }
 
-            var team_name =  {name: team.level, index: i};
+            var team_name = {name: team.level, index: i};
             team.gender == "Boys" ? year.boys.push(team_name) : year.girls.push(team_name)
         });
 
-        displayTeams(years);
+        // test
+        var years2 = [];
+        var index = 0;
+        years.forEach(function (year) {
+            for (var i = 0; i < year.boys.length || i < year.girls.length; i++) {
+                var year2 = {};
+                year2.even_row = index % 2;
+                if (i == 0) {
+                    year2.name = year.name;
+                } else {
+                    year2.name = "";
+                }
+
+                if (i < year.boys.length) year2.boys = year.boys[i];
+                if (i < year.girls.length) year2.girls = year.girls[i];
+
+                years2.push(year2);
+                index++;
+            }
+        });
+
+        displayTeams(years2);
 
     };
 
-    var processMapData = function(data) {
+    var processMapData = function (data) {
         var dates = {};
         // construct locations
         data.teams.forEach(function (team) {
@@ -287,7 +315,7 @@ var Teams = (function() {
                     if (!isNaN(lat) && !isNaN(lng)) {
 
                         var eventDate = new Date(event.start);
-                        var location = _locations.addLocation(lat,lng, event.location.address, event.location.name);
+                        var location = _locations.addLocation(lat, lng, event.location.address, event.location.name);
                         location.addGame(eventDate, teamName(team) + " vs " + event.opponent);
 
                         // save off date
@@ -298,23 +326,23 @@ var Teams = (function() {
         });
 
         var availableDates = [];
-        for(var date in dates) {
+        for (var date in dates) {
             availableDates.push(new Date(date));
         }
-        availableDates.sort(function(date1, date2) {
+        availableDates.sort(function (date1, date2) {
             // compare years first
             if (date1 > date2) return 1;
             if (date1 < date2) return -1;
             return 0;
         });
-        availableDates.forEach(function(item) {
+        availableDates.forEach(function (item) {
             var option = document.createElement('option');
             option.text = item.displayDate();
             option.value = item;
             _datePicker.add(option);
         });
 
-        _datePicker.addEventListener("change", function() {
+        _datePicker.addEventListener("change", function () {
             updateMap(new Date(this.value));
         });
 
@@ -329,15 +357,16 @@ var Teams = (function() {
 
     var modal;
     var modalCloseButton;
-    var openModal = function() {
+    var openModal = function () {
         if (modal == undefined) {
             modal = document.getElementById('teamModal');
             modalCloseButton = modal.getElementsByTagName('a')[0];
 
             // setup close handler
-            modalCloseButton.onclick = function() {
+            modalCloseButton.onclick = function (event) {
                 modal.style.opacity = 0;
                 modal.style.pointerEvents = "none";
+                event.preventDefault();
             };
         }
 
@@ -346,29 +375,30 @@ var Teams = (function() {
 
     };
 
-    var displayTeams = function(years) {
+    var displayTeams = function (years) {
         var teams = document.getElementById("teams");
         teams.innerHTML = teamsFn(years);
-	    var _teamId = document.getElementById("team");
+        var _teamId = document.getElementById("team");
 
         teams.addEventListener('click', function (event) {
-          var index = event.target.getAttribute('data-index');
-          if (index == undefined) return;
-          var team = _teams[index];
+            var index = event.target.getAttribute('data-index');
+            if (index == undefined) return;
+            var team = _teams[index];
 
-          openModal();
+            event.preventDefault();
+            openModal();
 
-          team.members.sort(function(member1, member2) {
-              if (member1.is_player > member2.is_player) return 1;
-              if (member1.is_player < member2.is_player) return -1;
+            team.members.sort(function (member1, member2) {
+                if (member1.is_player > member2.is_player) return 1;
+                if (member1.is_player < member2.is_player) return -1;
 
-              if (member1.name > member2.name) return 1;
-              if (member1.name < member2.name) return -1;
+                if (member1.name > member2.name) return 1;
+                if (member1.name < member2.name) return -1;
 
-              return 0;
-          });
+                return 0;
+            });
 
-          _teamId.innerHTML = teamFn(team);
+            _teamId.innerHTML = teamFn(team);
         });
     };
 
@@ -380,7 +410,7 @@ var Teams = (function() {
             markerCluster = new MarkerClusterer(_map, null, {imagePath: 'https://api.centralmarinsoccer.com/teams/static/images/m'});
         }
         // Clear existing markers
-        activeMarkers.forEach(function(marker) {
+        activeMarkers.forEach(function (marker) {
             marker.setMap(null);
         });
         activeMarkers = [];
@@ -389,7 +419,7 @@ var Teams = (function() {
         // Add markers for the specified date
         var newBoundary = new google.maps.LatLngBounds();
         var markers = _locations.markers(dateObj);
-        markers.forEach(function(marker) {
+        markers.forEach(function (marker) {
             marker.setMap(_map);
             newBoundary.extend(marker.position);
 
@@ -400,7 +430,7 @@ var Teams = (function() {
     };
 
     return {
-        initMap : initMap
+        initMap: initMap
     }
 })();
 
